35849f299932d2b5889ea2925d57a1194fd96da2
Jose Regalado <jlrmyt@gmail.com>
2021-11-03 14:47:16 -0500

    Fix on typo

--- utils/requests.js
+++ utils/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-    fetchTreding: {
+    fetchTrending: {
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     },
